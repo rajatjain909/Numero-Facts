c866110ed1b38c7788197f4a88aa20201f9c395d
rajatjain909 <rajatjain909@gmail.com>
2017-11-29 16:09:37 +0530

    All basic work done without UI

--- NumeroFacts/app/src/main/java/com/example/rsj/numerofacts/retrofact/interfaces/FetchFactServiceApi.java
+++ NumeroFacts/app/src/main/java/com/example/rsj/numerofacts/retrofact/interfaces/FetchFactServiceApi.java
@@ -13,19 +13,27 @@ import retrofit2.http.Path;
 
 public interface FetchFactServiceApi {
 
-    @GET("/random/date?json")
+    @GET("random/date?json")
     Call<ResultFact> getRandomDateFact();
 
-    @GET("/random/math?json")
+    @GET("random/math?json")
     Call<ResultFact> getRandomMathFact();
 
-    @GET("/random/trivia?json")
+    @GET("random/trivia?json")
     Call<ResultFact> getRandomTriviaFact();
 
-    @GET("/random/year?json")
+    @GET("random/year?json")
     Call<ResultFact> getRandomYearFact();
 
-    @GET("/{number}/math")
+    @GET("{month}/{date}/date")
+    Call<ResponseBody> getDateFact(@Path("month") int month, @Path("date") int date);
+
+    @GET("{number}/math")
     Call<ResponseBody> getMathFact(@Path("number") int number);
 
+    @GET("{number}/trivia")
+    Call<ResponseBody> getTriviaFact(@Path("number") int number);
+
+    @GET("{number}/year")
+    Call<ResponseBody> getYearFact(@Path("number") int number);
 }
